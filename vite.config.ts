06b2fa86cf8c3e22a9f7327799e16cfaa1003180
Vincent Douchin <vincent.douchin@gmail.com>
2023-10-13 21:38:06 +0200

    update pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,16 @@ export default defineConfig(() => {
 			registerType: 'autoUpdate',
 			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
 			manifest: {
-				name: 'My Awesome App',
-				short_name: 'MyApp',
-				description: 'My Awesome App description',
+				name: 'Time Loop RPG',
+				short_name: 'Time Loop RPG',
+				description: 'Time Loop RPG',
 				theme_color: '#ffffff',
 				icons: [
+					 {
+						src: 'pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png',
+					},
 					{
 						src: 'pwa-192x192.png',
 						sizes: '192x192',
@@ -25,6 +30,13 @@ export default defineConfig(() => {
 						src: 'pwa-512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
+						purpose: 'any',
+					},
+					{
+						src: 'maskable-icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
 					},
 				],
 			},
