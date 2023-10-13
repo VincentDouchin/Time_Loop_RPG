ee951207b6098e480335dda73cca30f683ea4537
Vincent Douchin <vincent.douchin@gmail.com>
2023-10-13 21:19:02 +0200

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
 import path from 'node:path'
 import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import watchAssets from './scripts/generateAssetNamesPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
 	const config: UserConfig = {
-		plugins: [watchAssets()],
+		plugins: [watchAssets(), VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+			manifest: {
+				name: 'My Awesome App',
+				short_name: 'MyApp',
+				description: 'My Awesome App description',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		})],
 		base: '',
 
 		build: {
