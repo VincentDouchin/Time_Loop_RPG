46dd7c49ce24427fb096dfbb8d5712ec0b16cacb
Vincent Douchin <vincent.douchin@gmail.com>
2023-10-15 17:09:35 +0200

    remove pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,53 +1,54 @@
 import path from 'node:path'
 import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
+
+// import { VitePWA } from 'vite-plugin-pwa'
 import watchAssets from './scripts/generateAssetNamesPlugin'
 
 export default defineConfig(() => {
 	const config: UserConfig = {
 		plugins: [
 			watchAssets(),
-			 VitePWA({
-				registerType: 'autoUpdate',
-				devOptions: {
-					enabled: true,
-				},
-				includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-				manifest: {
-					start_url: 'index.html?fullscreen=true',
-					display: 'fullscreen',
-					orientation: 'landscape',
-					name: 'Time Loop RPG',
-					short_name: 'TimeLoopRPG',
-					description: 'Time Loop RPG',
-					theme_color: '#000000',
-					icons: [
-						{
-							src: 'pwa-192x192.png',
-							sizes: '192x192',
-							type: 'image/png',
-						},
-						{
-							src: 'pwa-512x512.png',
-							sizes: '512x512',
-							type: 'image/png',
-						},
-						{
-							src: 'pwa-512x512.png',
-							sizes: '512x512',
-							type: 'image/png',
-							purpose: 'any',
-						},
-						{
-							src: 'pwa-512x512.png',
-							sizes: '512x512',
-							type: 'image/png',
-							purpose: 'maskable',
-						},
-					],
-				},
-			}),
+			//  VitePWA({
+			// 	registerType: 'autoUpdate',
+			// 	devOptions: {
+			// 		enabled: true,
+			// 	},
+			// 	includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+			// 	manifest: {
+			// 		start_url: 'index.html?fullscreen=true',
+			// 		display: 'fullscreen',
+			// 		orientation: 'landscape',
+			// 		name: 'Time Loop RPG',
+			// 		short_name: 'TimeLoopRPG',
+			// 		description: 'Time Loop RPG',
+			// 		theme_color: '#000000',
+			// 		icons: [
+			// 			{
+			// 				src: 'pwa-192x192.png',
+			// 				sizes: '192x192',
+			// 				type: 'image/png',
+			// 			},
+			// 			{
+			// 				src: 'pwa-512x512.png',
+			// 				sizes: '512x512',
+			// 				type: 'image/png',
+			// 			},
+			// 			{
+			// 				src: 'pwa-512x512.png',
+			// 				sizes: '512x512',
+			// 				type: 'image/png',
+			// 				purpose: 'any',
+			// 			},
+			// 			{
+			// 				src: 'pwa-512x512.png',
+			// 				sizes: '512x512',
+			// 				type: 'image/png',
+			// 				purpose: 'maskable',
+			// 			},
+			// 		],
+			// 	},
+			// }),
 		],
 		base: '',
 		build: {
