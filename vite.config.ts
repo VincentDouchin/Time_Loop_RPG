d2e8c637ce7ce01bc6b079640ba8c1470e46a559
Vincent Douchin <vincent.douchin@gmail.com>
2023-10-15 17:26:07 +0200

    replace config

--- vite.config.ts
+++ vite.config.ts
@@ -4,66 +4,66 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import watchAssets from './scripts/generateAssetNamesPlugin'
 
+// https://vitejs.dev/config/
 export default defineConfig(() => {
 	const config: UserConfig = {
-		plugins: [
-			watchAssets(),
-			 VitePWA({
-				registerType: 'autoUpdate',
-				devOptions: {
-					enabled: true,
-				},
-				includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-				manifest: {
-					start_url: 'index.html?fullscreen=true',
-					display: 'fullscreen',
-					orientation: 'landscape',
-					name: 'Time Loop RPG',
-					short_name: 'TimeLoopRPG',
-					description: 'Time Loop RPG',
-					theme_color: '#ffffff',
-					icons: [
-						{
-							src: 'pwa-192x192.png',
-							sizes: '192x192',
-							type: 'image/png',
-						},
-						{
-							src: 'pwa-512x512.png',
-							sizes: '512x512',
-							type: 'image/png',
-						},
-						{
-							src: 'pwa-512x512.png',
-							sizes: '512x512',
-							type: 'image/png',
-							purpose: 'any',
-						},
-						{
-							src: 'pwa-512x512.png',
-							sizes: '512x512',
-							type: 'image/png',
-							purpose: 'maskable',
-						},
-					],
-				},
-			}),
-		],
+		plugins: [watchAssets(), VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				start_url: 'index.html?fullscreen=true',
+				display: 'fullscreen',
+				orientation: 'landscape',
+				name: 'Hell\'s Tea Shop',
+				short_name: 'Hell\'s Tea Shop',
+				description: 'Hell\'s Tea Shop',
+				theme_color: '#000000',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+				],
+			},
+		})],
 		base: '',
+
 		build: {
 			target: 'esnext',
+
 		},
 		esbuild: {
 			jsxFactory: 'createUiEntity',
 			jsx: 'transform',
 			jsxInject: 'import { createUiEntity } from \"@/ui/JSXEntity\"',
 		},
+
 		resolve: {
 			alias: [
 				{ find: '@', replacement: path.resolve(__dirname, './src') },
+				{ find: '@assets', replacement: path.resolve(__dirname, './assets') },
 
 			],
 		},
+
 	}
+
 	return config
 })
